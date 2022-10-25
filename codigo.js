597ab84bc36e3feac23ef7b38eba473a9317af9c
1MarcoAntonio <xfiller93@gmail.com>
2022-10-24 21:21:10 -0300

    att

--- codigo.js
+++ codigo.js
@@ -2,7 +2,12 @@ function att(){
   return fetch('https://dummyjson.com/users/1')
   .then(res => res.json())
   .then(res => {
-    document.getElementById("user-name").innerHTML=res.users
+    const img = document.querySelector(".user-photo");
+    const div = document.querySelector(".div-head2")
+    img.src = res.image
+    
+ div.appendChild(img)
   })}
 
-  att();
+ att()
+
