99a1d296a8654fc691932a518d8ba1c3b74d6ea0
1MarcoAntonio <xfiller93@gmail.com>
2022-11-06 02:23:08 -0300

    fim do user

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 window.addEventListener('DOMContentLoaded', e =>{
-  e.preventDefault()
+  e.preventDefault();
 const main = document.querySelector("#main");
 
 async function userHeader(){
   await fetch('https://dummyjson.com/users/1')
   .then(res => res.json())
   .then(res => {
+    const nome = document.querySelector(".user-name");
     const img = document.querySelector(".user-photo");
     const div = document.querySelector(".div-head2");
-    img.src = res.image
-    div.appendChild(img)
+    const a = document.querySelector(".a-logo")
+    nome.textContent = res.firstName;
+    img.src = res.image;
+    a.appendChild(nome);
+    a.appendChild(img);
+    div.appendChild(a);
   })
  }
 
-async function usersAndImagens(imgUser, nameUser) {
+async function usersAndImagens(imgUser, nameUser ) {
   await fetch('https://dummyjson.com/users')
   .then(res => res.json())
   .then(({users})=>{
     let random = Math.floor(Math.random() * users.length); 
     const post = users[random];
       
-    imgUser.classList.add("user-photo1")
+    imgUser.classList.add("user-photo1");
     imgUser.src = post.image;
-    
-    nameUser.textContent = post.firstName
-    nameUser.classList.add('user-name-post')
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    imgUser.style.backgroundColor = "#" + randomColor;
+    nameUser.textContent = post.firstName;
+    nameUser.classList.add('user-name-post');
 
-    imgUser.addEventListener("click", ()=> sessionStorage.setItem("valor", post.id))
+    imgUser.addEventListener("click", ()=> sessionStorage.setItem("valor", post.id));
         
   
 })}
@@ -39,26 +45,35 @@ async function usersAndImagens(imgUser, nameUser) {
   .then(({posts}) =>{
       let  random = Math.floor(Math.random() * posts.length); 
       const post = posts[random];
-      const button = document.createElement("button")
-      const span = document.createElement("span")
+      const commentButton = document.createElement("button")
+      const spanComment = document.createElement("span")
+      const likeButton = document.createElement("button")
+      const spanLike = document.createElement("span")
       const divConteiner = document.createElement("div")
       const ancor = document.createElement("a")
       const imgUser = document.createElement('img')
+
       const nameUser = document.createElement('p')
       const title = document.createElement("p");
       title.textContent = post.title
       const postBody = document.createElement("p");
       postBody.textContent = post.body
       const imgPost = document.createElement("img")
-      
+      const divButton = document.createElement("div")
+
       divConteiner.classList.add("div-conteiner")
-      button.classList.add("button-like")
-      span.textContent = "comments"
+      divButton.classList.add("button")
+      likeButton.classList.add("button-like")
+      commentButton.classList.add("button-like")
+      spanLike.textContent = "Like"
+      spanComment.textContent = "Comments"
       ancor.href = "user.html"
-      button.appendChild(span)
+
+      likeButton.appendChild(spanLike)
+      commentButton.appendChild(spanComment)
       usersAndImagens(imgUser, nameUser)
 
-      button.addEventListener("click", ()=>{
+      commentButton.addEventListener("click", ()=>{
         comentarios(divConteiner)
       })
 
@@ -72,7 +87,9 @@ async function usersAndImagens(imgUser, nameUser) {
       divConteiner.appendChild(imgPost)
       divConteiner.appendChild(title)
       divConteiner.appendChild(postBody)
-      divConteiner.appendChild(button)
+      divButton.appendChild(likeButton)
+      divButton.appendChild(commentButton)
+      divConteiner.appendChild(divButton)
       main.appendChild(divConteiner)
   })
   }
@@ -102,9 +119,11 @@ async function usersAndImagens(imgUser, nameUser) {
     .then( res => {
       let random = Math.floor(Math.random() * res.comments.length); 
       const comment = res.comments[random];
-      const comentPost = document.createElement("p")
-      comentPost.textContent = comment.body
-      conteiner.appendChild(comentPost)
+      const commentPost = document.createElement("p")
+      commentPost.textContent = comment.body
+      commentPost.classList.add("comment")
+
+      conteiner.appendChild(commentPost)
   
   
   
@@ -127,17 +146,3 @@ userHeader()
   
 })
 
-
-
-
-
-
- 
-
- /* async function userPhotoPost() {
-  await fetch('https://dummyjson.com/users')
-  .then(res => res.json())
-  .then(res => {
-    
-  })
-} */
--- user.js
+++ user.js
@@ -1,12 +1,38 @@
 window.addEventListener('DOMContentLoaded', e =>{
-    e.preventDefault()
-const user = sessionStorage.getItem("valor")
+    e.preventDefault();
+const user = sessionStorage.getItem("valor");
+const main = document.querySelector("#main");
+const pNome = document.querySelector("#nome");
+const imgUser = document.querySelector("#user");
+const age = document.querySelector("#age");
+const birthDate = document.querySelector("#birth");
+const address = document.querySelector("#address");
+const city = document.querySelector("#city")
+const gender = document.querySelector("#gender");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+
+
+
 
 async function perfil(){
-    await fetch(`https://dummyjson.com/users/${parseInt(user)}`)
+    await fetch(`https://dummyjson.com/users/${user}`)
     .then(res => res.json())
     .then(res => {
+        pNome.textContent = res.firstName;
+        imgUser.src = res.image;
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        imgUser.style.backgroundColor = "#" + randomColor;
+        age.textContent = res.age;
+        gender.textContent = res.gender
+        birthDate.textContent = res.birthDate;
+        address.textContent = res.address.address;
+        city.textContent = res.address.city;
+        email.textContent = res.email;
+        phone.textContent = res.phone
 
     });
 }
+
+perfil()
 })
